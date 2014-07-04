faf400b636f4797d743d5aea4538d150519f1f20
Jero Sutlovic <jsutlovic@gmail.com>
2014-07-04 13:37:25 +0000

    gofmt on new files

--- handlers.go
+++ handlers.go
@@ -1,46 +1,46 @@
 package main
 
 import (
-    "fmt"
-    "github.com/gocraft/web"
-    "log"
-    "net/http"
+	"fmt"
+	"github.com/gocraft/web"
+	"log"
+	"net/http"
 )
 
 func (c *Context) TestHello(rw web.ResponseWriter, req *web.Request) {
-    fmt.Fprint(rw, "Hello, world!")
+	fmt.Fprint(rw, "Hello, world!")
 }
 
 func (c *Context) ApiAuth(rw web.ResponseWriter, req *web.Request) {
-    req.ParseForm()
+	req.ParseForm()
 
-    form := req.PostForm
-    //log.Println(form)
+	form := req.PostForm
+	//log.Println(form)
 
-    emails, email_ok := form["email"]
-    passwords, password_ok := form["password"]
-    if !(email_ok || password_ok) {
-        status := http.StatusBadRequest
-        rw.WriteHeader(status)
-        fmt.Fprintf(rw, "Email and password are required")
-        return
-    }
+	emails, email_ok := form["email"]
+	passwords, password_ok := form["password"]
+	if !(email_ok || password_ok) {
+		status := http.StatusBadRequest
+		rw.WriteHeader(status)
+		fmt.Fprintf(rw, "Email and password are required")
+		return
+	}
 
-    email := emails[0]
-    password := passwords[0]
+	email := emails[0]
+	password := passwords[0]
 
-    log.Print("Got email: ")
-    log.Println(email)
-    log.Print("Got password: ")
-    log.Print(password)
+	log.Print("Got email: ")
+	log.Println(email)
+	log.Print("Got password: ")
+	log.Print(password)
 
-    authed := CheckPassword(email, password)
-    if authed {
-        log.Println("Logged in!")
-        fmt.Fprintf(rw, "Successfully logged in")
-    } else {
-        log.Println("Nope.")
-        rw.WriteHeader(http.StatusUnauthorized)
-        fmt.Fprintf(rw, "Email or password is wrong")
-    }
+	authed := CheckPassword(email, password)
+	if authed {
+		log.Println("Logged in!")
+		fmt.Fprintf(rw, "Successfully logged in")
+	} else {
+		log.Println("Nope.")
+		rw.WriteHeader(http.StatusUnauthorized)
+		fmt.Fprintf(rw, "Email or password is wrong")
+	}
 }
--- middlewares.go
+++ middlewares.go
@@ -1,18 +1,16 @@
 package main
 
 import (
-    "github.com/gocraft/web"
+	"github.com/gocraft/web"
 )
 
 func (c *Context) AuthMiddleware(rw web.ResponseWriter, req *web.Request, next web.NextMiddlewareFunc) {
-    // Get API key
-    // Get Username
-    // check against database
-    next(rw, req)
+	// Get API key
+	// Get Username
+	// check against database
+	next(rw, req)
 }
 
 func (c *Context) TestMiddleware(rw web.ResponseWriter, req *web.Request, next web.NextMiddlewareFunc) {
-    next(rw, req)
+	next(rw, req)
 }
-
-
