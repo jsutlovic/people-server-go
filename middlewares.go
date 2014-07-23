8bd8c6f11bd94fa54689b61eb258c08f07c5fbe7
Jero Sutlovic <jsutlovic@gmail.com>
2014-07-23 23:14:54 +0000

    Don't print errors in middleware

--- middlewares.go
+++ middlewares.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"github.com/gocraft/web"
-	"log"
 	"net/http"
 )
 
@@ -28,29 +27,23 @@ If successful, sets a User to the current AuthContext
 func (c *AuthContext) AuthRequired(rw web.ResponseWriter, req *web.Request, next web.NextMiddlewareFunc) {
 	_, creds, err := GetAuthHeader(req.Request.Header)
 	if err != nil {
-		log.Println(err)
 		UnauthorizedHeader(rw)
 		return
 	}
 
 	email, apikey, err := ParseCredentials(creds)
 	if err != nil {
-		log.Println("Error: could not authenticate")
-		log.Println(err)
 		http.Error(rw, "Invalid authentication params", http.StatusBadRequest)
 		return
 	}
 
 	user, err := c.DB.GetUser(email)
 	if err != nil {
-		log.Println("Error: could not get user from auth")
-		log.Println(err)
 		http.Error(rw, "Invalid user", http.StatusForbidden)
 		return
 	}
 
 	if !user.CheckApiKey(apikey) {
-		log.Println("Error: API key does not match")
 		http.Error(rw, "Incorrect API key", http.StatusForbidden)
 		return
 	}
