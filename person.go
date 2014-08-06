875b5fa317b0b90f855eca9bc0e5c8bd1e0219ac
Jero Sutlovic <jsutlovic@gmail.com>
2014-08-06 00:12:41 +0000

    Implement Person struct and GetPerson

---
+++ person.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"database/sql"
+	"github.com/lib/pq/hstore"
+)
+
+type Person struct {
+	Id     int           `json:"id"`
+	UserId int           `db:"user_id" json:"user_id"`
+	Name   string        `json:"name"`
+	Meta   hstore.Hstore `json:"meta"`
+	Color  sql.NullInt64 `json:"color"`
+}
+
+func (s *pgDbService) GetPerson(id int) (*Person, error) {
+	person := new(Person)
+
+	err := s.db.Get(person, s.db.Rebind(`SELECT * FROM "person" WHERE id=?`), id)
+	if err != nil {
+		return nil, err
+	}
+	return person, nil
+}
