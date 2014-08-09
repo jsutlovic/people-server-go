0aa73e9e0d249804152c0fab657b745386115dab
Jero Sutlovic <jsutlovic@gmail.com>
2014-08-09 13:59:08 +0000

    Implement MarshalJSON/UnmarshalJSON for Person, no pointers in struct

--- person.go
+++ person.go
@@ -1,16 +1,79 @@
 package main
 
 import (
+	"bytes"
 	"database/sql"
+	"encoding/json"
 	"github.com/lib/pq/hstore"
 )
 
 type Person struct {
-	Id     int            `json:"id"`
-	UserId int            `db:"user_id" json:"user_id"`
-	Name   string         `json:"name"`
-	Meta   *hstore.Hstore `json:"meta"`
-	Color  *sql.NullInt64 `json:"color"`
+	Id     int
+	UserId int `db:"user_id"`
+	Name   string
+	Meta   hstore.Hstore
+	Color  sql.NullInt64
+}
+
+type personJSON struct {
+	Id     int               `json:"id"`
+	UserId int               `json:"user_id"`
+	Name   string            `json:"name"`
+	Meta   map[string]string `json:"meta"`
+	Color  json.RawMessage   `json:"color"`
+}
+
+func (p *Person) MarshalJSON() ([]byte, error) {
+	colorVal, _ := p.Color.Value()
+	colorJSON := []byte(Jsonify(colorVal))
+
+	metaVal := make(map[string]string)
+	if p.Meta.Map != nil {
+		for key, val := range p.Meta.Map {
+			if val.Valid {
+				metaVal[key] = val.String
+			}
+		}
+	}
+
+	pJson := personJSON{
+		Id:     p.Id,
+		UserId: p.UserId,
+		Name:   p.Name,
+		Meta:   metaVal,
+		Color:  colorJSON,
+	}
+
+	return json.Marshal(&pJson)
+}
+
+func (p *Person) UnmarshalJSON(b []byte) error {
+	pJson := new(personJSON)
+
+	err := json.Unmarshal(b, pJson)
+	if err != nil {
+		return err
+	}
+
+	p.Id = pJson.Id
+	p.UserId = pJson.UserId
+	p.Name = pJson.Name
+
+	p.Meta.Map = make(map[string]sql.NullString)
+
+	for key, val := range pJson.Meta {
+		p.Meta.Map[key] = sql.NullString{val, true}
+	}
+
+	if bytes.Equal(pJson.Color, []byte("null")) {
+		p.Color = sql.NullInt64{0, false}
+	} else {
+		var colorVal int64
+		json.Unmarshal(pJson.Color, &colorVal)
+		p.Color = sql.NullInt64{colorVal, true}
+	}
+
+	return nil
 }
 
 func (s *pgDbService) GetPerson(userId, id int) (*Person, error) {
