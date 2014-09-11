1055c27d6a741043c24c8e572d546578ba483225
Jero Sutlovic <jsutlovic@gmail.com>
2014-09-11 00:32:33 +0000

    Clean up setupRoutes and add new utilities

--- server.go
+++ server.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/gocraft/web"
 	"net/http"
+	"path"
 )
 
 type Config struct {
@@ -19,6 +20,11 @@ type Server struct {
 	routes     []PathRoute
 }
 
+type PrefixRouter struct {
+	router     *web.Router
+	pathPrefix string
+}
+
 type PathRoute struct {
 	Method  web.HttpMethod
 	Path    string
@@ -31,35 +37,61 @@ func NewServer(conf *Config) *Server {
 	return serv
 }
 
+func NewPrefixSubrouter(root *web.Router, prefix string, context interface{}) *PrefixRouter {
+	subrouter := root.Subrouter(context, prefix)
+	return &PrefixRouter{subrouter, prefix}
+}
+
+// Utility to map a web.HttpMethod to it's counterpart on web.Router
+func methodToRouter(method web.HttpMethod) func(*web.Router, string, interface{}) *web.Router {
+	switch method {
+	case web.HttpMethodGet:
+		return (*web.Router).Get
+	case web.HttpMethodPost:
+		return (*web.Router).Post
+	case web.HttpMethodPut:
+		return (*web.Router).Put
+	case web.HttpMethodDelete:
+		return (*web.Router).Delete
+	case web.HttpMethodPatch:
+		return (*web.Router).Patch
+	}
+	return nil
+}
+
+// Helper method to register a route with the router and server.routes
+func (s *Server) registerRoute(router *PrefixRouter, method web.HttpMethod, routePath string, handler interface{}) {
+	action := methodToRouter(method)
+	action(router.router, routePath, handler)
+	s.routes = append(s.routes, PathRoute{method, path.Join(router.pathPrefix, routePath), handler})
+}
+
 func (s *Server) setupRoutes() *web.Router {
 	rootRouter := web.New(Context{})
 	rootRouter.Middleware(web.LoggerMiddleware)
 	rootRouter.Middleware(web.ShowErrorsMiddleware)
 
-	authRouter := rootRouter.Subrouter(Context{}, "")
-	authRouter.Post("/auth", (*Context).ApiAuth)
-	s.routes = append(s.routes, PathRoute{web.HttpMethodPost, "/auth", (*Context).ApiAuth})
+	// Routers
+	authRouter := NewPrefixSubrouter(rootRouter, "", Context{})
 
 	// CreateUser endpoint cannot require auth
-	createUserRouter := rootRouter.Subrouter(Context{}, "")
-	createUserRouter.Post("/api/user", (*Context).CreateUserApi)
-	s.routes = append(s.routes, PathRoute{web.HttpMethodPost, "/api/user", (*Context).CreateUserApi})
+	createUserRouter := NewPrefixSubrouter(rootRouter, "", Context{})
 
 	// API subrouter for all other API endpoints
-	apiRouter := rootRouter.Subrouter(AuthContext{}, "/api")
-	apiRouter.Middleware((*AuthContext).AuthRequired)
+	apiRouter := NewPrefixSubrouter(rootRouter, "/api", AuthContext{})
+	apiRouter.router.Middleware((*AuthContext).AuthRequired)
+
+	// Routes
+	s.registerRoute(authRouter, web.HttpMethodPost, "/auth", (*Context).ApiAuth)
+	s.registerRoute(createUserRouter, web.HttpMethodPost, "/api/user", (*Context).CreateUserApi)
 
 	// User-related
-	apiRouter.Get("/user", (*AuthContext).GetUserApi)
-	s.routes = append(s.routes, PathRoute{web.HttpMethodGet, "/api/user", (*AuthContext).GetUserApi})
+	s.registerRoute(apiRouter, web.HttpMethodGet, "/user", (*AuthContext).GetUserApi)
 
 	// Person-related
-	apiRouter.Get("/person/:id:\\d+", (*AuthContext).GetPersonApi)
-	s.routes = append(s.routes, PathRoute{web.HttpMethodGet, "/api/person/:id", (*AuthContext).GetPersonApi})
-	apiRouter.Get("/person", (*AuthContext).GetPersonListApi)
-	s.routes = append(s.routes, PathRoute{web.HttpMethodGet, "/api/person", (*AuthContext).GetPersonListApi})
-	apiRouter.Post("/person", (*AuthContext).CreatePersonApi)
-	s.routes = append(s.routes, PathRoute{web.HttpMethodPost, "/api/person", (*AuthContext).CreatePersonApi})
+	s.registerRoute(apiRouter, web.HttpMethodGet, "/person/:id:\\d+", (*AuthContext).GetPersonApi)
+	s.registerRoute(apiRouter, web.HttpMethodGet, "/person", (*AuthContext).GetPersonListApi)
+	s.registerRoute(apiRouter, web.HttpMethodPost, "/person", (*AuthContext).CreatePersonApi)
 
 	return rootRouter
 }
