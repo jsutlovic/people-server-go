817af0ffdb4fc5c2d0a7d7cfe451933c334ac36d
Jero Sutlovic <jsutlovic@gmail.com>
2014-09-11 00:31:55 +0000

    Add tests for methodToRouter

--- server_test.go
+++ server_test.go
@@ -13,6 +13,42 @@ func TestServeNoConfig(t *testing.T) {
 	})
 }
 
+func TestMethodToRouter(t *testing.T) {
+	tests := []struct {
+		in  web.HttpMethod
+		out interface{}
+	}{
+		{
+			in:  web.HttpMethodGet,
+			out: (*web.Router).Get,
+		},
+		{
+			in:  web.HttpMethodPost,
+			out: (*web.Router).Post,
+		},
+		{
+			in:  web.HttpMethodPut,
+			out: (*web.Router).Put,
+		},
+		{
+			in:  web.HttpMethodDelete,
+			out: (*web.Router).Delete,
+		},
+		{
+			in:  web.HttpMethodPatch,
+			out: (*web.Router).Patch,
+		},
+		{
+			in:  web.HttpMethod("INFO"),
+			out: (func(*web.Router, string, interface{}) *web.Router)(nil),
+		},
+	}
+
+	for _, test := range tests {
+		assert.Equal(t, methodToRouter(test.in), test.out)
+	}
+}
+
 func TestSetupRoutes(t *testing.T) {
 	conf := newTestConfig()
 	serv := NewServer(conf)
@@ -23,7 +59,7 @@ func TestSetupRoutes(t *testing.T) {
 		PathRoute{web.HttpMethodPost, "/auth", (*Context).ApiAuth},
 		PathRoute{web.HttpMethodPost, "/api/user", (*Context).CreateUserApi},
 		PathRoute{web.HttpMethodGet, "/api/user", (*AuthContext).GetUserApi},
-		PathRoute{web.HttpMethodGet, "/api/person/:id", (*AuthContext).GetPersonApi},
+		PathRoute{web.HttpMethodGet, "/api/person/:id:\\d+", (*AuthContext).GetPersonApi},
 		PathRoute{web.HttpMethodGet, "/api/person", (*AuthContext).GetPersonListApi},
 		PathRoute{web.HttpMethodPost, "/api/person", (*AuthContext).CreatePersonApi},
 	}
