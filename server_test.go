9536b131672e1f894c56ebcb6da4ffef4b33e0d7
Jero Sutlovic <jsutlovic@gmail.com>
2015-01-13 17:50:02 -0500

    Switch to using our httpMethod type

--- server_test.go
+++ server_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"github.com/gocraft/web"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
@@ -13,42 +12,6 @@ func TestServeNoConfig(t *testing.T) {
 	})
 }
 
-func TestMethodToRouter(t *testing.T) {
-	tests := []struct {
-		in  web.HttpMethod
-		out interface{}
-	}{
-		{
-			in:  web.HttpMethodGet,
-			out: (*web.Router).Get,
-		},
-		{
-			in:  web.HttpMethodPost,
-			out: (*web.Router).Post,
-		},
-		{
-			in:  web.HttpMethodPut,
-			out: (*web.Router).Put,
-		},
-		{
-			in:  web.HttpMethodDelete,
-			out: (*web.Router).Delete,
-		},
-		{
-			in:  web.HttpMethodPatch,
-			out: (*web.Router).Patch,
-		},
-		{
-			in:  web.HttpMethod("INFO"),
-			out: (func(*web.Router, string, interface{}) *web.Router)(nil),
-		},
-	}
-
-	for _, test := range tests {
-		assert.Equal(t, methodToRouter(test.in), test.out)
-	}
-}
-
 func TestSetupRoutes(t *testing.T) {
 	conf := newTestConfig()
 	serv := NewServer(conf)
@@ -56,12 +19,12 @@ func TestSetupRoutes(t *testing.T) {
 	_ = serv.setupRoutes()
 
 	expectedRoutes := []PathRoute{
-		{web.HttpMethodPost, "/auth", (*Context).ApiAuth},
-		{web.HttpMethodPost, "/api/user", (*Context).CreateUserApi},
-		{web.HttpMethodGet, "/api/user", (*AuthContext).GetUserApi},
-		{web.HttpMethodGet, "/api/person/:id:\\d+", (*AuthContext).GetPersonApi},
-		{web.HttpMethodGet, "/api/person", (*AuthContext).GetPersonListApi},
-		{web.HttpMethodPost, "/api/person", (*AuthContext).CreatePersonApi},
+		{httpMethodPost, "/auth", (*Context).ApiAuth},
+		{httpMethodPost, "/api/user", (*Context).CreateUserApi},
+		{httpMethodGet, "/api/user", (*AuthContext).GetUserApi},
+		{httpMethodGet, "/api/person/:id:\\d+", (*AuthContext).GetPersonApi},
+		{httpMethodGet, "/api/person", (*AuthContext).GetPersonListApi},
+		{httpMethodPost, "/api/person", (*AuthContext).CreatePersonApi},
 	}
 
 	assert.Equal(t, serv.routes, expectedRoutes)
