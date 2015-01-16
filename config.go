759b22ab21fdda0d8b6d1c3957e098c530c1d843
Jero Sutlovic <jsutlovic@gmail.com>
2015-01-16 09:16:35 -0500

    Basic passing tests for ReadConfig

--- config.go
+++ config.go
@@ -1,12 +1,25 @@
 package main
 
+import (
+	"fmt"
+	"gopkg.in/yaml.v2"
+	"strings"
+)
+
 // Defaults
 const (
-	DefaultHost   = "127.0.0.1"
-	DefaultPort   = 3000
-	DefaultDbType = "postgres"
-	DefaultDbHost = "localhost"
-	DefaultDbPort = 5432
+	DefaultHost       = "127.0.0.1"
+	DefaultPort       = 3000
+	DefaultDbType     = "postgres"
+	DefaultDbHost     = "localhost"
+	DefaultDbPort     = 5432
+	DefaultSslMode    = "disable"
+	DbApplicationName = "people-go"
+)
+
+const (
+	KeyValTemplate     = "%s=%v"
+	ListenAddrTemplate = "%s:%d"
 )
 
 type Config interface {
@@ -22,6 +35,7 @@ type dbConfig struct {
 	User     string
 	Password string
 	DbName   string `yaml="name"`
+	SslMode  string `yaml="sslmode"`
 }
 
 type listenConfig struct {
@@ -35,14 +49,56 @@ type appConfig struct {
 }
 
 func (ac *appConfig) DbType() string {
-	return ""
+	return defaultString(ac.DbConf.Type, DefaultDbType)
 }
 
 func (ac *appConfig) DbCreds() string {
-	return ""
+	configStrings := []string{}
+
+	hostStr := fmt.Sprintf(
+		KeyValTemplate, "host", defaultString(ac.DbConf.Host, DefaultDbHost))
+
+	portStr := fmt.Sprintf(
+		KeyValTemplate, "port", defaultInt(ac.DbConf.Port, DefaultDbPort))
+
+	configStrings = append(configStrings, hostStr, portStr)
+
+	appNameStr := fmt.Sprintf(
+		KeyValTemplate, "application_name", DbApplicationName)
+
+	configStrings = append(configStrings, appNameStr)
+
+	return strings.Join(configStrings, " ")
 }
 
 func (ac *appConfig) ListenAddr() string {
-	return ""
+	return fmt.Sprintf(
+		ListenAddrTemplate,
+		defaultString(ac.ListenConf.Host, DefaultHost),
+		defaultInt(ac.ListenConf.Port, DefaultPort))
 }
 
+// Always returns a string. If chk is empty, returns def
+func defaultString(chk, def string) string {
+	if chk == "" {
+		return def
+	}
+	return chk
+}
+
+func defaultInt(chk, def int) int {
+	if chk == 0 {
+		return def
+	}
+	return chk
+}
+
+func ReadConfig(b []byte) (Config, error) {
+	config := &appConfig{}
+
+	err := yaml.Unmarshal(b, config)
+	if err != nil {
+		return nil, err
+	}
+	return config, nil
+}
